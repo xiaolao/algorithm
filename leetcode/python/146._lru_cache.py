fc1841aa560aadf92585b907f21fbb300cdb4b70
Jeffrey Chen <chenenquan0612@hotmail.com>
2019-02-06 20:14:30 +0800

    submit

---
+++ leetcode/python/146._lru_cache.py
@@ -0,0 +1,32 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        """
+        :type capacity: int
+        """
+        
+
+    def get(self, key: int) -> int:
+        """
+        :type key: int
+        :rtype: int
+        """
+        
+
+    def put(self, key: int, value: int):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+if __name__ == "__main__":
+    pass
+

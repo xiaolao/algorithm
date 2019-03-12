46f4b25c502a809a70ff1655c35c1beaed6ac4bb
chenenquan <chenenquan@qutoutiao.net>
2019-03-12 16:20:27 +0800

    add unit test.

---
+++ leetcode/python/two_sum_001.py
@@ -0,0 +1,28 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            if num in lookup:
+                return [lookup[num], i]
+            else:
+                lookup[target- num] = i
+        return None

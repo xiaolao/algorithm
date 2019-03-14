1a819537bc242d892d7bf8d6bca4180a2cdbc6b8
chenenquan <chenenquan@qutoutiao.net>
2019-03-14 20:34:26 +0800

    reverse link list in python

--- leetcode/python/206_reverse_linked_list.py
+++ leetcode/python/206_reverse_linked_list.py
@@ -9,28 +9,35 @@
 # A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 # Definition for singly-linked list.
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev, cur = None, head
         while cur:
             cur.next, prev, cur = prev, cur, cur.next
+        return prev
 
 
 if __name__ == "__main__":
-    head = ListNode(1)
-    cur = head
-    for val in range(2, 6):
-        node = ListNode(val)
-        cur.next = node
-        cur = node
+    def l2s(cur: ListNode) -> str:
+        vs = ""
+        while cur is not None:
+            vs += str(cur.val)
+            cur = cur.next
+        return vs
+    head = node1 = ListNode(1)
+    node1.next = node2 = ListNode(2)
+    node2.next = node3 = ListNode(3)
+    node3.next = node4 = ListNode(4)
+    node4.next = node5 = ListNode(5)
+    old = l2s(head)
     solution = Solution()
     head = solution.reverseList(head)
-    cur = head
-    while cur:
-        print(f"{cur.val} \n")
-        cur = cur.next
+    new = l2s(head)
+    assert old == new[::-1]

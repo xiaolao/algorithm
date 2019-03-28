1bae8b8c15b5cce9940ed48e1469f85a46832186
chenenquan <chenenquan0612@hotmail.com>
2019-03-28 15:36:21 +0800

    swap nodes in pairs in python.

---
+++ leetcode/scala/24SwapNodesInPairs.scala
@@ -0,0 +1,23 @@
+// Given a linked list, swap every two adjacent nodes and return its head.
+
+// You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+// Example:
+
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+
+object Solution {
+
+  def main(args: Array[String]) {
+  }
+
+  def swapPairs(head: ListNode): ListNode = {
+  }
+}

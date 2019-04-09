93c9064ae8f4afbd0b48ef34f9bbd64d067f6a88
chenenquan <chenenquan@qutoutiao.net>
2019-04-10 00:26:13 +0800

    [update] update reverse linked list in scala.

--- leetcode/scala/206ReverseLinkedList.scala
+++ leetcode/scala/206ReverseLinkedList.scala
@@ -10,13 +10,32 @@
 // Definition for singly-linked list.
 
 // Definition for singly-linked list.
+
 class ListNode(var _x: Int = 0) {
   var next: ListNode = null
   var x: Int = _x
 }
 
 object Solution {
-    def reverseList(head: ListNode): ListNode = {
-        
+
+  def main(args String[]) {
+  }
+
+  def reverseList(head: ListNode): ListNode = {
+    var pre: ListNode = null
+    var cur: ListNode = head
+    while(cur != null) {
+      cur.next, pre, cur = pre, cur, cur.next
     }
+    return pre
+  }
+
+  def Print(head: ListNode) String = {
+    var s: String = ""
+    var cur: ListNode = head
+    while(cur != null) {
+      s += cur.value
+      cur = cur.next
+    }
+  }
 }

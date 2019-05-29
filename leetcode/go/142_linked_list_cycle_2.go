c31506dd7e07e07ea271cf3aebc0580e502ca806
chenenquan <chenenquan@qutoutiao.net>
2019-05-30 01:03:01 +0800

    [add]

---
+++ leetcode/go/142_linked_list_cycle_2.go
@@ -0,0 +1,16 @@
+// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+// Note: Do not modify the linked list.
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+    
+}
---
+++ leetcode/java/141LinkedListCycle.java
@@ -0,0 +1,44 @@
+// Given a linked list, determine if it has a cycle in it.
+
+// To represent a cycle in the given linked list, we use an integer
+// pos which represents the position (0-indexed) in the linked list
+// where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        assert(hasCycle(node1) == false);
+        node5.next = node2;
+        assert(hasCycle(node2) == true);
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

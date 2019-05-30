b4d9de3e3e676784a84f7fc6af6871e66778a5e2
chenenquan <chenenquan@qutoutiao.net>
2019-05-30 21:02:14 +0800

    [add] add linked list cycle II in go

--- leetcode/go/142_linked_list_cycle_2.go
+++ leetcode/go/142_linked_list_cycle_2.go
@@ -4,6 +4,8 @@
 
 // Note: Do not modify the linked list.
 
+package leetcode
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -12,5 +14,18 @@
  * }
  */
 func detectCycle(head *ListNode) *ListNode {
-    
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow, fast := head.Next, head.Next.Next
+	for fast != nil && fast.Next != nil && slow != fast {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	if slow != fast {
+		return nil
+	}
+	for slow != head {
+		slow, head = slow.Next, head.Next
+	}
+	return slow
 }

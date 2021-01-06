ef528f38b44f569e05747344bcd74f9db61fe34c
xiaolao <chenenquan0612@hotmail.com>
2021-01-06 17:13:08 +0800

    098 valid bst in c++

---
+++ leetcode/cpp/098/TEST.cc
@@ -0,0 +1,48 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch2/catch.hh"
+#include "solution.h"
+#include <vector>
+
+
+TEST_CASE("098", "validateBinarySearchTree")
+{
+
+    // Solution s;
+    // {
+    //     TreeNode root = TreeNode(3);
+    //     TreeNode node_1 = TreeNode(2);
+    //     TreeNode node_2 = TreeNode(3);
+    //     TreeNode node_4 = TreeNode(3);
+    //     TreeNode node_6 = TreeNode(1);
+
+    //     root.left = &node_1;
+    //     root.right = &node_2;
+    //     node_1.right = &node_4;
+    //     node_2.right = &node_6;
+
+    //     REQUIRE( (s.rob1(&root) == 7) );
+    //     REQUIRE( (s.rob2(&root) == 7) );
+    //     REQUIRE( (s.rob(&root)) == 7 );
+
+    // }
+
+    // {
+    //     TreeNode root = TreeNode(3);
+    //     TreeNode node_1 = TreeNode(4);
+    //     TreeNode node_2 = TreeNode(5);
+    //     TreeNode node_3 = TreeNode(1);
+    //     TreeNode node_4 = TreeNode(3);
+    //     TreeNode node_6 = TreeNode(1);
+
+    //     root.left = &node_1;
+    //     root.right = &node_2;
+    //     node_1.left = &node_3;
+    //     node_1.right = &node_4;
+    //     node_2.right = &node_6;
+    //     REQUIRE( (s.rob1(&root) == 9) );
+    //     REQUIRE( (s.rob2(&root) == 9) );
+    //     REQUIRE( (s.rob(&root) == 9) );
+
+    // }
+
+}

5eb0107c75a463159657441d85fc720545cd2d52
xiaolao <chenenquan0612@hotmail.com>
2021-02-25 17:41:37 +0800

    add 69 sqrt(x) in c++

--- leetcode/cpp/69/solution.h
+++ leetcode/cpp/69/solution.h
@@ -18,3 +18,52 @@
 // Constraints:
 
 // 0 <= x <= 231 - 1
+
+/**
+ * 牛顿迭代法
+ * 令y = f(x) = x ^^ 2 - C  则f(x)在xi处的一阶泰勒展开为 f(x) ~ f(xi) + f(xi)`(x - xi)
+ */
+
+#include <cmath>
+
+using std::log;
+using std::exp;
+using std::fabs;
+
+class Solution {
+public:
+
+    // 牛顿迭代法
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        double C = x, x0 = x, xi = 0.0;
+        while (true) {
+            xi = 0.5 * (x0 + C / x0);
+            if (fabs(xi - x0) < 1e-7) break;
+            x0 = xi;
+        }
+        return int(x0);
+    }
+
+    // 二分查找法
+    int mySqrt1(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    // 袖珍计算器算法
+    int mySqrt2(int x) {
+        if (x == 0) return 0;
+        int ans = exp(0.5 * log(x));
+        return (long long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+};

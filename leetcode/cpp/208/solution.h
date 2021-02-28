19ed395557ebc05584117d72a741899a7386835f
xiaolao <chenenquan0612@hotmail.com>
2021-02-28 13:43:00 +0800

    add 208 trie in c++

---
+++ leetcode/cpp/208/solution.h
@@ -0,0 +1,76 @@
+// Implement a trie with insert, search, and startsWith methods.
+
+// Example:
+
+// Trie trie = new Trie();
+
+// trie.insert("apple");
+// trie.search("apple");   // returns true
+// trie.search("app");     // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");   
+// trie.search("app");     // returns true
+// Note:
+// You may assume that all inputs are consist of lowercase letters a-z.
+// All inputs are guaranteed to be non-empty strings.
+
+#include <cstring>
+#include <string>
+
+using std::memset;
+using std::string;
+
+
+class Trie {
+private:
+    bool isEnd;
+    Trie* next[26];
+
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        isEnd = false;
+        memset(next, 0, sizeof(next));
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node = this;
+        for (char c : word) {
+            if (node->next[c-'a'] == nullptr) {
+                node->next[c-'a'] = new Trie();
+            }
+            node = node->next[c-'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node = this;
+        for (char c : word) {
+            node = node->next[c-'a'];
+            if (node == nullptr) return false;
+        }
+        return node->isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char c : prefix) {
+            node = node->next[c-'a'];
+            if (node == nullptr) return false;
+        }
+        return true;
+    }
+
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
